65fb35f6035d621248fdf2bb5863a7f5e45e3911
Brobss <gabrieldomingos13@gmail.com>
2023-04-12 11:56:02 -0300

    Pagina Inicial: Produtos

--- screens/Home.js
+++ screens/Home.js
@@ -6,56 +6,56 @@ export default function App() {
   const [filmes, setFilmes] = React.useState([
     {
       id: 1,
-      nome: "Mauro",
-      genero: "Aventura",
-      capa: "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/02/super_mario_bros_filme_poster__2b3rnu52-758x1200.jpeg",
+      nome: "Bicicleta",
+      preco: "10000,00",
+      capa: "https://m.media-amazon.com/images/S/aplus-media-library-service-media/e34cebad-ca71-4de2-a4d1-b74d5e7168cf.__CR0,0,970,600_PT0_SX970_V1___.png",
     },
     {
       id: 2,
-      nome: "Meu Gay Favorito",
-      genero: "Biografia",
-      capa: "https://static.wikia.nocookie.net/meu-malvado-favorito/images/f/f1/Image1.jpg/revision/latest?cb=20130724012419&path-prefix=pt-br",
+      nome: "Among Action Figure",
+      preco: "95,50",
+      capa: "https://ae01.alicdn.com/kf/H7a083e0513714932a2578cca8a4aac75x.jpg?width=1920&height=1920&hash=3840",
     },
     {
       id: 3,
       nome: "Morbius",
-      genero: "Bom",
+      preco: "Bom",
       capa: "https://pbs.twimg.com/media/FFyT_6GVgAAGgrG?format=jpg&name=900x900",
     },
     {
       id: 4,
       nome: "Sharknado 5",
-      genero: "Tubação",
+      preco: "Tubação",
       capa: "https://m.media-amazon.com/images/M/MV5BMjQ3Mzk5NzAwNV5BMl5BanBnXkFtZTgwNDkwOTc3MjI@._V1_FMjpg_UX1000_.jpg",
     },
     {
       id: 5,
       nome: "O Biscoito Assassino",
-      genero: "Nem sei",
+      preco: "Nem sei",
       capa: "https://media.fstatic.com/STk4Yah-zE0ROG-KNjTZFvF3yQw=/322x478/smart/filters:format(webp)/media/movies/covers/2010/06/1145f96568edd14ede57b28bf156369f.jpg",
     },
     {
       id: 6,
       nome: "Vivemos em uma Sociedade que",
-      genero: "Drama",
+      preco: "Drama",
       capa: "https://img.elo7.com.br/product/original/2A1A4B7/big-poster-filme-joker-coringa-joaquin-phoenix-tam-90x60-cm-nerd.jpg",
     },
     {
       id: 7,
       nome: "Among Us",
-      genero: "Terror / Mistério",
+      preco: "Terror / Mistério",
       capa: "https://upload.wikimedia.org/wikipedia/pt/f/f9/Thing_1982.png",
     },
     {
       id: 8,
       nome: "Deltarune O Filme",
-      genero: "Muitos",
+      preco: "Muitos",
       capa: "https://i.etsystatic.com/31113100/r/il/858258/3577034661/il_794xN.3577034661_lvnd.jpg",
     },
     {
       id: 9,
       nome: "Tubarão de 6 cabeças",
-      genero: "Nem me pergunte",
+      preco: "Nem me pergunte",
       capa: "https://media.fstatic.com/sXKpFVatQNEju1RsLcNnKgc9xOo=/322x478/smart/filters:format(webp)/media/movies/covers/2018/05/20180509_222042.png",
     },
   ]);
@@ -68,14 +68,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
+      {/* <View style={styles.header}>
         <Text style={styles.titulo}>Filminhos</Text>
-      </View>
-      <ScrollView>
+      </View> */}
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
         {filmes.map((filme) => (
           <Card
             nome={filme.nome}
-            genero={filme.genero}
+            preco={filme.preco}
             capa={filme.capa}
             removerItem={() => removerItem(filme.id)}
             key={filme.id}
@@ -88,21 +88,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
   },
-  header: {
-    backgroundColor: "red",
-    color: "white",
-    fontSize: 30,
-    margin: 10,
-    padding: 20,
-    width: "100%",
-    alignContent: "center",
-    alignItems: "center",
-  },
+  // header: {
+  //   backgroundColor: "red",
+  //   color: "white",
+  //   fontSize: 30,
+  //   margin: 10,
+  //   padding: 20,
+  //   width: "100%",
+  //   alignContent: "center",
+  //   alignItems: "center",
+  // },
   titulo: {
     color: "white",
     fontSize: 30,
+  },
+  scrollViewContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    paddingHorizontal: 5,
   },
 });
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,54 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons/faCartShopping";
 
 export default function Card(props) {
   return (
     <View style={styles.card}>
       <Image source={{ uri: props.capa }} style={styles.capa} />
-      <Text style={styles.nomeFilme}>{props.nome}</Text>
-      <Text style={styles.genero}>Gênero(s): {props.genero}</Text>
+      <Text style={styles.nomeProduto}>{props.nome}</Text>
+      <Text style={styles.preco}>R$ {props.preco}</Text>
       <TouchableOpacity onPress={props.removerItem} style={styles.botao}>
-        <Text style={{ color: "white", fontWeight: "bold" }}>Deletar</Text>
+        <Text style={{ color: "#464545", fontWeight: "bold" }}>
+          <FontAwesomeIcon icon={faCartShopping} /> +
+        </Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  nomeFilme: {
-    color: "white",
-    fontSize: 30,
+  nomeProduto: {
+    color: "#e0e0e0",
+    fontSize: 20,
+    padding: 3,
   },
   card: {
-    backgroundColor: "black",
+    backgroundColor: "#464545",
     color: "white",
     borderRadius: 10,
     fontSize: 30,
-    margin: 10,
-    padding: 20,
-    alignItems: "center",
-    width: 200,
+    padding: 5,
+    width: "48%",
+    margin: "1%",
   },
   capa: {
     resizeMode: "cover",
-    width: 200,
-    height: 300,
+    width: "100%",
+    aspectRatio: 1,
+    borderRadius: 5,
   },
-  genero: {
-    color: "grey",
-    fontSize: 15,
+  preco: {
+    color: "white",
+    fontSize: 30,
+    padding: 3,
   },
   botao: {
-    backgroundColor: "red",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    backgroundColor: "#F0F3B0",
+    padding: 10,
     borderRadius: 5,
-    margin: 10,
+    margin: 5,
+    width: "30%",
+    alignSelf: "flex-end",
   },
 });
