8c7b48af5b780b6b3ef3a538d64338c5ea1b629e
PauloK3tchup <passandoalimpo13@gmail.com>
2023-08-22 16:41:53 -0300

    Alert carrinho

--- src/screens/Produto/index.js
+++ src/screens/Produto/index.js
@@ -5,11 +5,40 @@ import {
   ScrollView,
   Image,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons/faCartShopping";
 import { useNavigation } from "@react-navigation/native";
+
+const showAlertCarrinho = () =>
+  Alert.alert(
+    "Adicionar ao Carrinho",
+    "Deseja mesmo adicionar ao carrinho?",
+    [
+      {
+        text: "Cancelar",
+        onPress: () =>
+          Alert.alert(
+            "Operação Cancelada",
+            "O Produto não foi adicionado ao Carrinho!"
+          ),
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: () =>
+          Alert.alert("Pronto!", "Produto Adicionado ao Carrinho!"),
+      },
+    ],
+    {
+      cancelable: true,
+      onDismiss: () => {
+        console.log("Dismissed");
+      },
+    }
+  );
 
 export default function Produto({ route, navigation }) {
   const { item } = route.params;
@@ -28,7 +57,7 @@ export default function Produto({ route, navigation }) {
         <TouchableOpacity style={styles.btnCompra}>
           <Text>Comprar Agora</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btnCarro}>
+        <TouchableOpacity onPress={showAlertCarrinho} style={styles.btnCarro}>
           <FontAwesomeIcon icon={faCartShopping} />
           <Text>+ Add ao Carrinho</Text>
         </TouchableOpacity>
